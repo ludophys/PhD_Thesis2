f9725039c3ec662752896c7dda47ecb8381c9a0e
ludophys <ludo.donn.wk@gmail.com>
2026-07-22 10:52:03 +0200

    automatisation of the process for several runs

--- GanEss/1.4keV/xenon2/noise_analysis.sh
+++ GanEss/1.4keV/xenon2/noise_analysis.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
+run_array=("2661" "2662" "2663" "2664")
 
-echo "Run number ?"
-read run_nb
-
-echo "What is the gas (Xe or Ar) ?"
-read gas
+for run_nb in "${run_array[@]}"; do
+    
+gas="Xe"
+evt_start="0"
+f_numb="200"
+wd_func="coif"
 
 echo "The selected gas is : $gas"
 
 echo "The selected run number is : $run_nb"
 
-echo "Number of starting event ?"
-read evt_start
-
-echo "File number ?"
-read f_numb
-
 echo "Number of file is : $f_numb"
 
 echo "The analysis starts at the event number : $evt_start"
 
-echo "What is the wavelet function to use ?"
-read wd_func
 
 echo "The selected wavelet function is : $wd_func"
 
@@ -30,3 +24,6 @@ python fluct_study.py $run_nb $evt_start $wd_func $gas $f_numb
 python time_generation.py $run_nb $evt_start $wd_func $gas $f_numb
 python charge_study.py $run_nb $evt_start $wd_func $gas $f_numb
 #python charge_total.py $run_nb $evt_start $wd_func $gas $f_numb
+
+
+done
